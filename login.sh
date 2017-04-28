f0b8a0d56eaad90b9e7b85c9501da76bfa817b74
Jeffrey Kwong <jkwong@Jeffreys-MacBook-Pro.local>
2017-04-28 15:50:48 -0400

    login.sh

--- login.sh
+++ login.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export BLUEMIX_API_KEY=`cat /var/run/secrets/bluemix-api-key`
+export BLUEMIX_API_KEY=`cat /var/run/secrets/bluemix-api-key/api-key`
 
 bx login -c ${BX_ORG} -s ${BX_SPACE}
 
