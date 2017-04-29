1d961b2c9aa685a20e297020c8c94b0dbe107544
Jeffrey Kwong <jkwong@Jeffreys-MacBook-Pro.local>
2017-04-29 14:16:38 -0400

    update cluster deployer

--- bluemix-cluster-deploy/login.sh
+++ bluemix-cluster-deploy/login.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ -f "/var/run/secrets/bluemix-api-key/api-key" ]; then
-    BLUEMIX_API_KEY=`cat /var/run/secrets/bluemix-api-key/api-key`
-fi
-
-bx login -a ${BX_API_ENDPOINT} -s ${BX_SPACE}
-
+source /bx_login.sh 
 # initialize docker using container registry secret
 bx cs init
 
--- bx_login.sh
+++ bx_login.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ -f "/var/run/secrets/bluemix-api-key/api-key" ]; then
+    echo "Loading Bluemix API key from /var/run/secrets/bluemix-api-key/api-key ..."
     export BLUEMIX_API_KEY=`cat /var/run/secrets/bluemix-api-key/api-key`
 fi
 
